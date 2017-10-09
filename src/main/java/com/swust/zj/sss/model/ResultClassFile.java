ecd703866a03350184a21bd05803a267994fe7b8
周杰 <shixi_zhoujie@staff.weibo.com>
2017-10-09 14:05:04 +0800

    v1.0

---
+++ src/main/java/com/swust/zj/sss/model/ResultClassFile.java
@@ -0,0 +1,156 @@
+package com.swust.zj.sss.model;
+
+import com.swust.zj.template.JavaFile;
+
+/**
+* @author 周杰
+* @time 2017年10月9日 上午10:46:57
+*/
+public class ResultClassFile extends JavaFile {
+	private static final String CLASS_NAME = "Result";
+	private static final String FILE_NAME = "Result.java";
+	public ResultClassFile(String fileDir, String mainPackage, String curPackage) {
+		super(fileDir, FILE_NAME, mainPackage+ "."+curPackage, CLASS_TYPE, CLASS_NAME);
+		
+		String resource = 
+				"import com.fasterxml.jackson.databind.JsonNode;\r\n" + 
+				"import com.fasterxml.jackson.databind.ObjectMapper;\r\n" + 
+				"import java.util.List;\r\n" + 
+				"\r\n" + 
+				"/**\r\n" + 
+				" * Created by Zhou Jie on 2017/5/24.\r\n" + 
+				" */\r\n" + 
+				"public class Result {\r\n" + 
+				"    private static final Integer SUCCESS_STATUS = 200;\r\n" + 
+				"    private static final Integer FAILURE_STATUS = 400;\r\n" + 
+				"    private static final Integer INTERNAL_ERROR_STATUS = 500;\r\n" + 
+				"    private static final String SUCCESS_MESSAGE = \"success\";\r\n" + 
+				"    private static final String FAILURE_MESSAGE = \"failure\";\r\n" + 
+				"    private static final String INTERNAL_ERROR_MESSAGE = \"internal error\";\r\n" + 
+				"    //状态\r\n" + 
+				"    private Integer status;\r\n" + 
+				"\r\n" + 
+				"    //消息\r\n" + 
+				"    private String message;\r\n" + 
+				"\r\n" + 
+				"    //数据\r\n" + 
+				"    private Object data;\r\n" + 
+				"\r\n" + 
+				"    //构造方法\r\n" + 
+				"    private Result(Integer status, String message, Object data) {\r\n" + 
+				"        this.status = status;\r\n" + 
+				"        this.message = message;\r\n" + 
+				"        this.data = data;\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //无数据的成功结果\r\n" + 
+				"    public static Result success(){\r\n" + 
+				"        return new Result(SUCCESS_STATUS,SUCCESS_MESSAGE,null);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //有数据的成功结果\r\n" + 
+				"    public static Result success(Object data){\r\n" + 
+				"        return new Result(SUCCESS_STATUS,SUCCESS_MESSAGE,data);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //无消息的失败结果\r\n" + 
+				"    public static Result failure(){\r\n" + 
+				"        return new Result(FAILURE_STATUS,FAILURE_MESSAGE,null);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //有消息的失败结果\r\n" + 
+				"    public static Result failure(String message){\r\n" + 
+				"        return new Result(FAILURE_STATUS,message,null);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //无消息的内部异常结果\r\n" + 
+				"    public static Result internalError(){\r\n" + 
+				"        return new Result(INTERNAL_ERROR_STATUS,INTERNAL_ERROR_MESSAGE,null);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //有消息的内部异常结果\r\n" + 
+				"    public static Result internalError(String message){\r\n" + 
+				"        return new Result(INTERNAL_ERROR_STATUS,message,null);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //自定义构建结果\r\n" + 
+				"    public static Result build(Integer status,String message,Object data){\r\n" + 
+				"        return new Result(status,message,data);\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //设置和获取成员变量的方法\r\n" + 
+				"    public Integer getStatus() {\r\n" + 
+				"        return status;\r\n" + 
+				"    }\r\n" + 
+				"    public void setStatus(Integer status) {\r\n" + 
+				"        this.status = status;\r\n" + 
+				"    }\r\n" + 
+				"    public String getMessage() {\r\n" + 
+				"        return message;\r\n" + 
+				"    }\r\n" + 
+				"    public void setMessage(String message) {\r\n" + 
+				"        this.message = message;\r\n" + 
+				"    }\r\n" + 
+				"    public Object getData() {\r\n" + 
+				"        return data;\r\n" + 
+				"    }\r\n" + 
+				"    public void setData(Object data) {\r\n" + 
+				"        this.data = data;\r\n" + 
+				"    }\r\n" + 
+				"\r\n" + 
+				"    //json与数据转化核心类\r\n" + 
+				"    private static final ObjectMapper MAPPER = new ObjectMapper();\r\n" + 
+				"\r\n" + 
+				"    //没有数据的转化\r\n" + 
+				"    public static Result format(String json) {\r\n" + 
+				"        try {\r\n" + 
+				"            return MAPPER.readValue(json, Result.class);\r\n" + 
+				"        } catch (Exception e) {\r\n" + 
+				"            e.printStackTrace();\r\n" + 
+				"        }\r\n" + 
+				"        return null;\r\n" + 
+				"    }\r\n" + 
+				"    \r\n" + 
+				"    //数据是对象的转化\r\n" + 
+				"    public static Result formatToPojo(String json, Class<?> clazz) {\r\n" + 
+				"        try {\r\n" + 
+				"            if (clazz == null) {\r\n" + 
+				"                return MAPPER.readValue(json, Result.class);\r\n" + 
+				"            }\r\n" + 
+				"            JsonNode jsonNode = MAPPER.readTree(json);\r\n" + 
+				"            JsonNode data = jsonNode.get(\"data\");\r\n" + 
+				"            Object obj = null;\r\n" + 
+				"            if (clazz != null) {\r\n" + 
+				"                if (data.isObject()) {\r\n" + 
+				"                    obj = MAPPER.readValue(data.traverse(), clazz);\r\n" + 
+				"                } else if (data.isTextual()) {\r\n" + 
+				"                    obj = MAPPER.readValue(data.asText(), clazz);\r\n" + 
+				"                }\r\n" + 
+				"            }\r\n" + 
+				"            return build(jsonNode.get(\"status\").intValue(), jsonNode.get(\"message\").asText(), obj);\r\n" + 
+				"        } catch (Exception e) {\r\n" + 
+				"            e.printStackTrace();\r\n" + 
+				"        }\r\n" + 
+				"        return null;\r\n" + 
+				"    }\r\n" + 
+				"    \r\n" + 
+				"    //数据是集合的转化\r\n" + 
+				"    public static Result formatToList(String jsonData, Class<?> clazz) {\r\n" + 
+				"        try {\r\n" + 
+				"            JsonNode jsonNode = MAPPER.readTree(jsonData);\r\n" + 
+				"            JsonNode data = jsonNode.get(\"data\");\r\n" + 
+				"            Object obj = null;\r\n" + 
+				"            if (data.isArray() && data.size() > 0) {\r\n" + 
+				"                obj = MAPPER.readValue(data.traverse(),\r\n" + 
+				"                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));\r\n" + 
+				"            }\r\n" + 
+				"            return build(jsonNode.get(\"status\").intValue(), jsonNode.get(\"message\").asText(), obj);\r\n" + 
+				"        } catch (Exception e) {\r\n" + 
+				"            e.printStackTrace();\r\n" + 
+				"        }\r\n" + 
+				"        return null;\r\n" + 
+				"    }\r\n" + 
+				"}";
+		super.init(resource);
+	}
+}
---
+++ src/main/java/com/swust/zj/template/JavaFile.java
@@ -0,0 +1,331 @@
+package com.swust.zj.template;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+* @author 周杰
+* @time 2017年9月27日 上午11:52:46
+*/
+public class JavaFile {
+	protected static final String CLASS_TYPE = "class";
+	protected static final String INTERFACE_TYPE = "interface";
+	protected String fileDir;
+	protected String fileName;
+	protected String packagei;
+	protected String type;
+	protected String name;
+	protected Collection<String> imports;
+	protected Collection<String> classAnnotations;
+	protected Collection<String> extendss;
+	protected Collection<String> implementss;
+	protected Collection<Field> fields;
+	protected Collection<Method> methods;
+	
+	protected String resource;
+	
+	protected static class Field{
+    	Collection<String> annotations;
+        String modifier;
+        String type;
+        String name;
+        String value;
+        public Field(Collection<String> annotations, String modifier, String type, String name, String value){
+            this.annotations = annotations;
+            this.modifier = modifier;
+            this.type = type;
+            this.name = name;
+            this.value = value;
+        }
+    }
+	protected static class Method{
+    	Collection<String> annotations;
+        String modifier;
+        String resultType;
+        String name;
+        String args;
+        Collection<String> exceptions;
+        String methodBody;
+        public Method(Collection<String> annotations, String modifier, String resultType, String name, String args, Collection<String> exceptions, String methodBody){
+            this.annotations = annotations;
+            this.modifier = modifier;
+            this.resultType = resultType;
+            this.name = name;
+            this.args = args;
+            this.exceptions = exceptions;
+            this.methodBody = methodBody;
+        }
+    }
+    public JavaFile(String fileDir, String fileName, String packagei, String type, String name) {
+		super();
+		if(!type.equals("class") && !type.equals("interface")) {
+			throw new IllegalArgumentException();
+		}
+		this.fileDir = fileDir;
+		this.fileName = fileName;
+		this.packagei = packagei;
+		this.type = type;
+		this.name = name;
+	}
+    protected void init(String resource) {
+    	this.resource = resource;
+    }
+    protected void init(Collection<String> imports,
+    		Collection<String> classAnnotations,
+			Collection<String> extendss,
+    		Collection<String> implementss,
+    		Collection<Field> fields,
+    		Collection<Method> methods) {
+    	this.imports = imports;
+    	this.classAnnotations = classAnnotations;
+    	this.extendss = extendss;
+    	this.implementss = implementss;
+    	this.fields = fields;
+    	this.methods = methods;
+    }
+    protected File makeJavaDir() throws Exception{
+        synchronized (JavaFile.class) {
+            File dir = new File(fileDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            return dir;
+        }
+    }
+    protected File makeJavaFile() throws Exception{
+        synchronized (JavaFile.class) {
+            File dir = makeJavaDir();
+            if (!fileName.endsWith(".java")) {
+                fileName += ".java";
+            }
+            File file = new File(dir, fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            return file;
+        }
+    }
+    public void generate() throws Exception{
+        //startWrite
+        File file = makeJavaFile();
+        FileWriter fileWriter = new FileWriter(file);
+        //initString
+        StringBuilder stringBuilder = new StringBuilder();
+        //ensureWritePackage
+        stringBuilder.append("package ");
+        stringBuilder.append(packagei);
+        stringBuilder.append(";\n");
+        //ensureWriteImport
+        if(imports != null) {
+            Iterator<String> iterator = imports.iterator();
+            while (iterator.hasNext()) {
+                stringBuilder.append("import ");
+                stringBuilder.append(iterator.next());
+                stringBuilder.append(";\n");
+            }
+        }
+        //writeClassAnnotations
+        if(classAnnotations != null){
+            Iterator<String> iterator = classAnnotations.iterator();
+            while(iterator.hasNext()){
+            	stringBuilder.append("@");
+                stringBuilder.append(iterator.next());
+                stringBuilder.append("\n");
+            }
+        }
+        //ensureWriteClassStart
+        stringBuilder.append("public ");
+        stringBuilder.append(type);
+        stringBuilder.append(" ");
+        stringBuilder.append(name);
+        stringBuilder.append(" ");
+        //ensureWriteExtends
+        if(extendss != null) {
+	        Iterator<String> iterator = extendss.iterator();
+	        int count = 0;
+	        while(iterator.hasNext()) {
+	        	if(count==0) {
+	        		stringBuilder.append("extends ");
+	        	}else {
+	        		stringBuilder.append(",");
+	        	}
+	        	stringBuilder.append(iterator.next());
+	        	count++;
+	        }
+	        if(count > 0) {
+	        	stringBuilder.append(" ");
+	        }
+        }
+        //ensureWritesImplements
+        if(implementss != null) {
+	        Iterator<String> iterator = implementss.iterator();
+	        int count = 0;
+	        while(iterator.hasNext()) {
+	        	if(count==0) {
+	        		stringBuilder.append("implements ");
+	        	}else {
+	        		stringBuilder.append(",");
+	        	}
+	        	stringBuilder.append(iterator.next());
+	        	count++;
+	        }
+	        if(count > 0) {
+	        	stringBuilder.append(" ");
+	        }
+        }
+        stringBuilder.append("{\n");
+        //ensureWriteField
+        if(fields != null) {
+            Iterator<Field> iterator = fields.iterator();
+            while (iterator.hasNext()) {
+                Field field = iterator.next();
+                if(field.annotations!=null){
+                    Iterator<String> iteratorAnnotations = field.annotations.iterator();
+                    while(iteratorAnnotations.hasNext()) {
+                        stringBuilder.append("\t");
+                        stringBuilder.append("@");
+                        stringBuilder.append(iteratorAnnotations.next());
+                        stringBuilder.append("\n");
+                    }
+                }
+                if(field.modifier!=null){
+                    stringBuilder.append("\t");
+                    stringBuilder.append(field.modifier);
+                    stringBuilder.append(" ");
+                }
+                if(field.type!=null){
+                    stringBuilder.append(field.type);
+                    stringBuilder.append(" ");
+                }
+                if(field.name!=null){
+                    stringBuilder.append(field.name);
+                }
+                if(field.value!=null){
+                    stringBuilder.append(" = ");
+                    stringBuilder.append(field.value);
+                }
+                stringBuilder.append(";\n");
+            }
+        }
+        //ensureWriteMethod
+        if(type.equals("class")) {
+	        if(methods != null) {
+	            Iterator<Method> iterator = methods.iterator();
+	            while (iterator.hasNext()) {
+	                Method method = iterator.next();
+	                if(method.annotations!=null){
+	                    Iterator<String> iteratorAnnotations = method.annotations.iterator();
+	                    while(iteratorAnnotations.hasNext()) {
+	                        stringBuilder.append("\t");
+	                        stringBuilder.append("@");
+	                        stringBuilder.append(iteratorAnnotations.next());
+	                        stringBuilder.append("\n");
+	                    }
+	                }
+	                if(method.modifier!=null){
+	                    stringBuilder.append("\t");
+	                    stringBuilder.append(method.modifier);
+	                    stringBuilder.append(" ");
+	                }
+	                if(method.resultType!=null){
+	                    stringBuilder.append(method.resultType);
+	                    stringBuilder.append(" ");
+	                }
+	                if(method.name!=null){
+	                    stringBuilder.append(method.name);
+	                }
+	                stringBuilder.append("(");
+	                if(method.args!=null){
+	                    stringBuilder.append(method.args);
+	                }
+	                stringBuilder.append(")");
+	                if(method.exceptions!=null){
+	                    Iterator<String> iteratorExceptions = method.exceptions.iterator();
+	                    int count = 0;
+	                    while(iteratorExceptions.hasNext()) {
+	                        if(count == 0){
+	                            stringBuilder.append(" throws ");
+	                        }else{
+	                            stringBuilder.append(",");
+	                        }
+	                        stringBuilder.append(iteratorExceptions.next());
+	                        count++;
+	                    }
+	                }
+	                stringBuilder.append("{\n");
+	                if(method.methodBody!=null){
+	                    stringBuilder.append(method.methodBody);
+	                }
+	                stringBuilder.append("\t}\n");
+	            }
+	        }
+        }else if(type.equals("interface")) {
+	        if(methods != null) {
+	            Iterator<Method> iterator = methods.iterator();
+	            while (iterator.hasNext()) {
+	                Method method = iterator.next();
+	                if(method.modifier!=null){
+	                    stringBuilder.append("\t");
+	                    stringBuilder.append(method.modifier);
+	                    stringBuilder.append(" ");
+	                }
+	                if(method.resultType!=null){
+	                    stringBuilder.append(method.resultType);
+	                    stringBuilder.append(" ");
+	                }
+	                if(method.name!=null){
+	                    stringBuilder.append(method.name);
+	                    stringBuilder.append("(");
+	                }
+	                if(method.args!=null){
+	                    stringBuilder.append(method.args);
+	                    stringBuilder.append(")");
+	                }
+	                if(method.exceptions!=null){
+	                    Iterator<String> iteratorExceptions = method.exceptions.iterator();
+	                    int count = 0;
+	                    while(iteratorExceptions.hasNext()) {
+	                        if(count == 0){
+	                            stringBuilder.append(" throws ");
+	                        }else{
+	                            stringBuilder.append(",");
+	                        }
+	                        stringBuilder.append(iteratorExceptions.next());
+	                        count++;
+	                    }
+	                }
+	                stringBuilder.append(";\n");
+	            }
+	        }
+        }
+        //ensureWriteClassEnd
+        stringBuilder.append("}");
+        //writeAll
+        fileWriter.write(stringBuilder.toString());
+        //endWrite
+        fileWriter.flush();
+        fileWriter.close();
+    }
+    public void personalGenerate() throws Exception {
+    	//startWrite
+        File file = makeJavaFile();
+        FileWriter fileWriter = new FileWriter(file);
+        //initString
+        StringBuilder stringBuilder = new StringBuilder();
+        //ensureWritePackage
+        stringBuilder.append("package ");
+        stringBuilder.append(packagei);
+        stringBuilder.append(";\r\n");
+        //addResource
+        if(resource != null) {
+        	stringBuilder.append(resource);
+        }
+        //writeAll
+        fileWriter.write(stringBuilder.toString());
+        //endWrite
+        fileWriter.flush();
+        fileWriter.close();
+    }
+}
